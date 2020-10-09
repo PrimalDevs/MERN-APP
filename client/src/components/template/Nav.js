33f9d77881e82c7a3e10b5b4ae67ca475a6fcee6
Edwin López <edwin.lopezb.1297@cotecnova.edu.co>
2020-10-09 00:25:02 -0500

    small changes

--- client/src/components/template/Nav.js
+++ client/src/components/template/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Nav = ({ setToggleButton, toggleButton }) => {
 
@@ -24,10 +24,10 @@ const Nav = ({ setToggleButton, toggleButton }) => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="nav navbar-nav ml-auto">
-                        {/* <li className="nav-item active">
-                            <a className="nav-link" href="#">Page</a>
-                        </li>
                         <li className="nav-item">
+                            <Link className="nav-link" to="/logout">LogOut</Link>
+                        </li>
+                        {/* <li className="nav-item">
                             <a className="nav-link" href="#">Page</a>
                         </li> */}
                     </ul>
